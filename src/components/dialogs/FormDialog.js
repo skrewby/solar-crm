ac97416b02c7336400bb06e357ea1502339d8a08
Pedro Alves <github@overthemil.com>
2022-10-19 10:48:37 +1100

    Reset form on close and disable submit button on error

--- src/components/dialogs/FormDialog.js
+++ src/components/dialogs/FormDialog.js
@@ -9,17 +9,22 @@ import { FormFields } from 'components/form/FormFields';
 // ==============================|| FORM DIALOG ||============================== //
 
 export default function FormDialog({ open, onClose, title, formik, formikFields, submitText = 'Submit' }) {
+  const closeForm = () => {
+    formik.resetForm();
+    onClose();
+  };
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog open={open} onClose={closeForm}>
       <Box sx={{ p: 1, py: 1.5 }}>
         <DialogTitle>{title}</DialogTitle>
         <DialogContent>{<FormFields formik={formik} fields={formikFields} width={12} />}</DialogContent>
         <DialogActions>
-          <Button color="error" onClick={onClose}>
+          <Button color="error" onClick={closeForm}>
             Cancel
           </Button>
           <Button
             variant="contained"
+            disabled={!formik.isValid || !formik.dirty}
             onClick={() => {
               formik.handleSubmit();
               onClose();
